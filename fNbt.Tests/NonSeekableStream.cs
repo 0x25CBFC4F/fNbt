29274141aeee6e41c00d58373fa823983ddc6e12
0x25CBFC4F <9310d27e@gmail.com>
2022-04-14 23:10:25 +0300

    Added ported Test project

---
+++ fNbt.Tests/NonSeekableStream.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace fNbt.Tests;
+
+internal class NonSeekableStream : Stream
+{
+    private readonly Stream stream;
+
+
+    public NonSeekableStream(Stream baseStream)
+    {
+        stream = baseStream;
+    }
+
+
+    public override bool CanRead => stream.CanRead;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => stream.CanWrite;
+
+
+    public override long Length => throw new NotSupportedException();
+
+    public override long Position
+    {
+        get => throw new NotSupportedException();
+        set => throw new NotSupportedException();
+    }
+
+
+    public override void Flush()
+    {
+        stream.Flush();
+    }
+
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return stream.Read(buffer, offset, count);
+    }
+
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        stream.Write(buffer, offset, count);
+    }
+}
---
+++ fNbt.Tests/PartialReadStream.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace fNbt.Tests;
+
+internal class PartialReadStream : Stream
+{
+    private readonly Stream baseStream;
+    private readonly int increment;
+
+    public PartialReadStream([NotNull] Stream baseStream)
+        : this(baseStream, 1)
+    {
+    }
+
+
+    public PartialReadStream([NotNull] Stream baseStream, int increment)
+    {
+        if (baseStream == null)
+        {
+            throw new ArgumentNullException(nameof(baseStream));
+        }
+
+        this.baseStream = baseStream;
+        this.increment = increment;
+    }
+
+
+    public override bool CanRead => true;
+
+    public override bool CanSeek => baseStream.CanSeek;
+
+    public override bool CanWrite => false;
+
+    public override long Length => baseStream.Length;
+
+    public override long Position
+    {
+        get => baseStream.Position;
+        set => baseStream.Position = value;
+    }
+
+
+    public override void Flush()
+    {
+        baseStream.Flush();
+    }
+
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return baseStream.Seek(offset, origin);
+    }
+
+
+    public override void SetLength(long value)
+    {
+        baseStream.SetLength(value);
+    }
+
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        var bytesToRead = Math.Min(increment, count);
+        return baseStream.Read(buffer, offset, bytesToRead);
+    }
+
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+}
